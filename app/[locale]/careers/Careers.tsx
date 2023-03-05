dbc906c06ce8f4a0f62af0e35f99af70e5665c2a
Cali Castle <hi@cali.so>
2023-03-05 23:30:03 +0800

    fix: build error

--- app/[locale]/careers/Careers.tsx
+++ app/[locale]/careers/Careers.tsx
@@ -1,20 +1,17 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
+import Balancer from 'react-wrap-balancer'
 
 export function Careers() {
   const t = useTranslations('Careers')
 
   return (
     <>
-      <h1>{t('Heading')}</h1>
-      <p>
-        We're on a mission to build next-generation applications, we need your
-        wizardry to make it happen. If you're a believer in the power of
-        crafting enchanting software experiences, we want to chat with you.
-        Let's conjure up something extraordinary together - apply now and let
-        the magic begin!
-      </p>
+      <h1>
+        <Balancer>{t('Heading')}</Balancer>
+      </h1>
+      <p>{t('Intro')}</p>
     </>
   )
 }
