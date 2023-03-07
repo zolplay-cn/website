6441123c50a11a4ac199d8b0d427891876e8a732
Cali Castle <hi@cali.so>
2023-03-07 11:30:13 +0800

    fix: page transitions

--- app/[locale]/template.tsx
+++ app/[locale]/template.tsx
@@ -5,8 +5,6 @@ import { motion } from 'framer-motion'
 export default function RootTemplate({ children }) {
   return (
     <motion.div
-      layout="position"
-      layoutId="root"
       initial={{ opacity: 0, y: 12 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 12 }}
