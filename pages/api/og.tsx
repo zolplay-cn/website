e78c5ec7dda1a7ca5c42c8f2f4bfa10172671c0f
Cali Castle <hi@cali.so>
2023-03-08 01:49:50 +0800

    fix: og background image src

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -29,7 +29,7 @@ export default async function handler(req: NextRequest) {
           flexDirection: 'column',
           alignItems: 'flex-start',
           justifyContent: 'center',
-          backgroundImage: `url(http://localhost:3333/assets/og-bg-${locale}.jpg)`,
+          backgroundImage: `url(https://zolplay.com/assets/og-bg-${locale}.jpg)`,
         }}
       >
         <div
