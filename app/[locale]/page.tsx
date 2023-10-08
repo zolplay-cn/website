5bcd23299daafbbf1aabd78f080f513984df38c3
Zheng Bang-Bo <zhengbangbo@hotmail.com>
2023-10-09 07:51:19 +0800

    chore: add home image

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+import Fuwu from './aidbo-fuwu.png'
 
 export default function Home() {
   const t = useTranslations('Home')
@@ -9,6 +11,15 @@ export default function Home() {
     <>
       <h1>👋&nbsp;{t('Heading')}</h1>
       <p>{t.rich('Paragraph1')}</p>
+      <a
+        href="https://fuwu.weixin.qq.com/profile/sq_oWTzf6t7Vrzf099ONaqXS5vMc0L4"
+        target="_blank"
+      >
+        <Image src={Fuwu} alt="Aidbo Fuwu" placeholder="blur" />
+      </a>
+      <p className="-mt-4 text-center text-sm text-stone-400 dark:text-stone-500">
+        {t('Caption')}
+      </p>
     </>
   )
 }
