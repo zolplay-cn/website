07b53f023b89936e22e332d2c1af7db59eb60993
Cali Castle <hi@cali.so>
2025-03-24 15:55:09 +0800

    refactor: update typography styles and improve component rendering in MDX

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -117,7 +117,7 @@ export default async function RootLayout({ children, params }: { children: React
                 <Rulers />
                 <Sidebar />
                 <section className='flex relative z-20 flex-col flex-auto p-5 pb-36 mt-3 w-full bg-white border border-transparent shadow-xl frosted-noise dark:border-stone-800  dark:bg-[#1a1a1a] md:mt-0 md:p-7 md:pb-36 lg:p-9 lg:pb-44'>
-                  <article className='prose dark:prose-invert prose-headings:tracking-tighter prose-h1:text-2xl prose-p:leading-loose prose-p:tracking-tight prose-li:tracking-tight prose-img:rounded-xl lg:prose-h1:text-4xl'>
+                  <article className='prose dark:prose-invert prose-headings:tracking-[-0.05em] prose-headings:font-medium prose-h1:text-2xl prose-p:leading-[1.85em] prose-p:tracking-tight prose-li:tracking-tight prose-img:rounded-xl lg:prose-h1:text-3xl prose-strong:font-medium prose-strong:text-black prose-strong:dark:text-white'>
                     {children}
                   </article>
                   <Footer />
