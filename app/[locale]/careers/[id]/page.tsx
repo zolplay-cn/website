d6eebf07e170c79bde68ff28fcbd015329ed2030
Cali Castle <hi@cali.so>
2025-04-03 17:38:10 +0800

    refactor: temporarily remove job fetching and metadata generation, redirecting to careers page

--- app/[locale]/careers/[id]/page.tsx
+++ app/[locale]/careers/[id]/page.tsx
@@ -1,39 +1,38 @@
-import type { Metadata } from 'next'
 import type { RootParams } from '~/types/app'
-import React from 'react'
-import { getJob } from '~/lib/ashbyhq.queries'
-import { Job } from '~/modules/careers/job'
 import { redirect } from '~/modules/i18n/navigation'
 
 type PageParams = RootParams & Promise<{ id: string }>
 
-export async function generateMetadata({ params }: { params: PageParams }): Promise<Metadata> {
-  const { id } = await params
-  const job = await getJob(id)
-
-  if (!job) {
-    return {}
-  }
-
-  const title = job.title
-
-  return {
-    title,
-    description: job.descriptionSocial,
-    openGraph: {
-      title,
-      description: job.descriptionSocial,
-    },
-  }
+// temp: removing all jobs
+export async function generateStaticParams() {
+  return []
 }
 
+// export async function generateMetadata({ params }: { params: PageParams }): Promise<Metadata> {
+//   const { id } = await params
+//   const job = await getJob(id)
+
+//   if (!job) {
+//     return {}
+//   }
+
+//   const title = job.title
+
+//   return {
+//     title,
+//     description: job.descriptionSocial,
+//     openGraph: {
+//       title,
+//       description: job.descriptionSocial,
+//     },
+//   }
+// }
+
+// temp: redirect to careers page
 export default async function JobPage({ params }: { params: PageParams }) {
-  const { id, locale } = await params
-  const job = await getJob(id)
+  const { locale } = await params
 
-  if (!job) {
-    redirect({ href: '/careers', locale })
-  }
+  redirect({ href: '/careers', locale })
 
-  return <Job job={job} />
+  // return <Job job={job} />
 }
