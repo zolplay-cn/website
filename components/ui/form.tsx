b26fbad6d06acb7fb7edc5515eee716b63e2ef1c
Cuvii <i@cuvii.dev>
2025-04-05 14:34:24 +0800

    refactor: remove unnecessary ref prop from Input and TextArea components

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -84,11 +84,10 @@ Label.displayName = 'Form.Label'
 const input =
   'block w-full rounded-lg border-0 bg-transparent py-1.5 px-2.5 ring-1 ring-inset ring-stone-300 placeholder:text-stone-400 focus:ring-2 focus:ring-inset focus:ring-stone-400 dark:ring-stone-700 dark:placeholder:text-stone-600 dark:focus:ring-stone-500 text-sm sm:leading-6'
 
-function Input({ ref, className, id, name, type, ...rest }: ComponentProps<'input'>) {
+function Input({ className, id, name, type, ...rest }: ComponentProps<'input'>) {
   const { name: defaultName } = use(FieldGroupContext)
   return (
     <input
-      ref={ref}
       className={clsxm(input, className)}
       id={id ?? defaultName ?? name}
       name={defaultName ?? name}
@@ -102,11 +101,10 @@ function Input({ ref, className, id, name, type, ...rest }: ComponentProps<'inpu
 }
 Input.displayName = 'Form.Input'
 
-function TextArea({ ref, className, id, name, ...rest }: ComponentProps<'textarea'>) {
+function TextArea({ className, id, name, ...rest }: ComponentProps<'textarea'>) {
   const { name: defaultName } = use(FieldGroupContext)
   return (
     <textarea
-      ref={ref}
       className={clsxm(input, className, 'p-2.5')}
       id={id ?? defaultName ?? name}
       name={defaultName ?? name}
