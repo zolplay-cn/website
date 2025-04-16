cb779f44cfda4cd51ef09879ea0ed9232b2ea2a9
Cali Castle <hi@cali.so>
2025-04-16 16:41:16 +0800

    feat: integrate Balancer component and add new image to MDX pages
    - Added Balancer component to MDX components for improved text wrapping.
    - Introduced 2023-hangout.jpg image to English and Chinese MDX pages to enhance visual content.
    - Updated about page content to reflect a more concise introduction to Zolplay.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks-extra/no-unnecessary-use-prefix */
 import type { MDXComponents } from 'mdx/types'
 import NextImage from 'next/image'
+import Balancer from 'react-wrap-balancer'
 import * as otherComponents from './components/mdx'
 import { Link } from './modules/i18n/navigation'
 
@@ -8,6 +9,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     ...otherComponents,
+    Balancer,
     Link,
     Image: NextImage,
   }
