7d53aee3beeeb91e02bcfdf2927d2df8a958995c
Cali Castle <hi@cali.so>
2025-04-16 16:22:47 +0800

    feat: enhance MDX components and update content with new images
    - Added Link component to MDX components for improved navigation.
    - Introduced new images (tagline.jpg and neuship.jpg) to enhance visual content in English and Chinese pages.
    - Updated content in page.en.mdx and page.zh-CN.mdx to reflect new branding and service offerings, including Neuship.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,11 +2,13 @@
 import type { MDXComponents } from 'mdx/types'
 import NextImage from 'next/image'
 import * as otherComponents from './components/mdx'
+import { Link } from './modules/i18n/navigation'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     ...otherComponents,
+    Link,
     Image: NextImage,
   }
 }
