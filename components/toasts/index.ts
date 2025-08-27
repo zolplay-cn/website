8e95b2a52d7918f870ec224388d4bc3ee7e3cf22
Cali Castle <hi@cali.so>
2025-08-28 00:31:48 +0800

    fix: build error

--- components/toasts/index.ts
+++ components/toasts/index.ts
@@ -1,6 +1,6 @@
 'use client'
 import dynamic from 'next/dynamic'
 
-export const Toasts = dynamic(() => import('./toasts.jsx').then((mod) => mod.Toasts), {
+export const Toasts = dynamic(() => import('./toasts.tsx').then((mod) => mod.Toasts), {
   ssr: false,
 })
