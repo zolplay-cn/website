178272403a170a401d5e6b70f65fd8a3bb6331ba
Cali Castle <hi@cali.so>
2025-12-31 17:52:02 +0800

    refactor(footer): inline zh-CN contact details in ICPRecord (#108)

--- components/icp-record.tsx
+++ components/icp-record.tsx
@@ -18,8 +18,12 @@ export function ICPRecord() {
   if (!shouldShow) return null
 
   return (
-    <a className='my-2 text-xs opacity-50' href='https://beian.miit.gov.cn/' target='_blank' rel='noreferrer'>
-      粤ICP备2025454293号
-    </a>
+    <div className='mb-4 flex flex-col items-center gap-1 text-xs opacity-50'>
+      <a className='my-1' href='https://beian.miit.gov.cn/' target='_blank' rel='noreferrer'>
+        粤ICP备2025454293号
+      </a>
+      <span>公司办公地址：深圳市南山区高新南七道软件园 T3 栋四楼 A03</span>
+      <span>联系电话：18923441049</span>
+    </div>
   )
 }
